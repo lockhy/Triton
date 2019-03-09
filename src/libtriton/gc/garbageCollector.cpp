dd16b86f33b095068b7e47fe176dc16c4ff7e44d
Jonathan Salwan <jonathan.salwan@gmail.com>
2019-03-09 11:57:30 +0100

    wip

--- src/libtriton/gc/garbageCollector.cpp
+++ src/libtriton/gc/garbageCollector.cpp
@@ -20,11 +20,35 @@ namespace triton {
 
 
     GarbageCollector::GarbageCollector() {
+      this->end = false;
+      this->t = std::thread(&GarbageCollector::threadRelease, this);
     }
 
 
     GarbageCollector::~GarbageCollector() {
-      this->loopRelease();
+      bool stop = false;
+
+      /* Tell to the thread that we are going to dead */
+      this->end = true;
+
+      /* waits for the thread to finish its execution */
+      this->t.join();
+
+      /*
+       * This part of the code is processed in order to release garbages
+       * until there is nothing to release anymore.
+       */
+      while (stop == false) {
+        stop = true;
+        while (this->expressions.size()) {
+          this->release();
+          stop = false;
+        }
+        while (this->nodes.size()) {
+          this->release();
+          stop = false;
+        }
+      }
     }
 
 
@@ -44,7 +68,9 @@ namespace triton {
         }
 
         if (node.use_count() == 1) {
+          this->m1.lock();
           this->nodes.insert(node);
+          this->m1.unlock();
         }
       }
     }
@@ -63,27 +89,20 @@ namespace triton {
         if (node->getType() == triton::ast::REFERENCE_NODE) {
           const auto& expr = reinterpret_cast<triton::ast::ReferenceNode*>(node.get())->getSymbolicExpression();
           if (expr.use_count() == 1) {
+            this->m2.lock();
             this->expressions.insert(expr);
+            this->m2.unlock();
           }
         }
       }
     }
 
 
-    void GarbageCollector::loopRelease(void) {
-      bool stop = false;
-
-      /* Garbage collect while it's needed */
-      while (stop == false) {
-        stop = true;
-        while (this->expressions.size()) {
-          this->release();
-          stop = false;
-        }
-        while (this->nodes.size()) {
-          this->release();
-          stop = false;
-        }
+    void GarbageCollector::threadRelease(void) {
+      /* This loop is processed in a thread while GarbageCollector is alive */
+      while (this->end == false) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        this->release();
       }
     }
 
@@ -92,8 +111,13 @@ namespace triton {
       std::set<triton::ast::SharedAbstractNode> garbageNodes;
       std::set<triton::engines::symbolic::SharedSymbolicExpression> garbageExpressions;
 
+      this->m1.lock();
       std::swap(garbageNodes, this->nodes);
+      this->m1.unlock();
+
+      this->m2.lock();
       std::swap(garbageExpressions, this->expressions);
+      this->m2.unlock();
 
       //std::cout << "Release " << garbageNodes.size() << " nodes" << std::endl;
       //std::cout << "Release " << garbageExpressions.size() << " expressions" << std::endl;
--- src/libtriton/includes/triton/garbageCollector.hpp
+++ src/libtriton/includes/triton/garbageCollector.hpp
@@ -8,7 +8,10 @@
 #ifndef TRITON_GARBAGE_COLLECTOR_HPP
 #define TRITON_GARBAGE_COLLECTOR_HPP
 
+#include <chrono>
+#include <mutex>
 #include <set>
+#include <thread>
 
 #include <triton/ast.hpp>
 #include <triton/dllexport.hpp>
@@ -34,6 +37,22 @@ namespace triton {
     //! \class GarbageCollector
     /*! \brief This class is used to garbage collector unused AbstractNode and SymbolicExpressions. */
     class GarbageCollector {
+      private:
+        //! Internal mutex used for nodes.
+        std::mutex m1;
+
+        //! Internal mutex used for expressions.
+        std::mutex m2;
+
+        //! Internal thread to release garbages.
+        std::thread t;
+
+        //! This flag is false while GarbageCollector is alive. ~GarbageCollector sets this flag to true.
+        bool end;
+
+        //! Releases garbages in a thread while GarbageCollector is alive.
+        void threadRelease(void);
+
       public:
         //! The list of AbstractNode which must be deleted.
         std::set<triton::ast::SharedAbstractNode> nodes;
@@ -41,23 +60,20 @@ namespace triton {
         //! The list of SymbolicExpression which must be deleted.
         std::set<triton::engines::symbolic::SharedSymbolicExpression> expressions;
 
-        //! Constructor
+        //! Constructor.
         TRITON_EXPORT GarbageCollector();
 
-        //! Destructor
+        //! Destructor.
         TRITON_EXPORT ~GarbageCollector();
 
-        //! Collects an AbstractNode
+        //! Collects an AbstractNode.
         TRITON_EXPORT void collect(triton::ast::AbstractNode* node);
 
-        //! Collects an SymbolicExpression
+        //! Collects an SymbolicExpression.
         TRITON_EXPORT void collect(triton::engines::symbolic::SymbolicExpression* expr);
 
         //! Releases the current collected garbages.
         TRITON_EXPORT void release(void);
-
-        //! Releases garbages until there is nothing to release anymore.
-        TRITON_EXPORT void loopRelease(void);
     };
 
     //! The global garbage collector instance used while the library is alive.
